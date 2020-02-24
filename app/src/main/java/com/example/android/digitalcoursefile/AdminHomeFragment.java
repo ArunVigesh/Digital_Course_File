0ba010f957881c69677ad4d3750f2867e08c9f14
ArunVigesh <arunvigesh1999@gmail.com>
2020-02-24 12:35:02 +0530

    initial commit

--- app/src/main/java/com/example/android/digitalcoursefile/AdminHomeFragment.java
+++ app/src/main/java/com/example/android/digitalcoursefile/AdminHomeFragment.java
@@ -1,17 +1,88 @@
 package com.example.android.digitalcoursefile;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
 
 public class AdminHomeFragment extends Fragment {
+    ArrayList dataList = new ArrayList<userApproval>();
+    View view;
+    UserApprovalAdapter mAdapter;
+    RecyclerView approval;
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_admin_home, container, false);
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.fragment_admin_home, container, false);
+         approval= view.findViewById(R.id.userApproval);
+
+        AsyncHttpClient myClient = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        myClient.post("https://dcfse.000webhostapp.com/userApproval.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Log.e("ER",new String(responseBody));
+                JSONObject jsonObject;
+                try {
+                    jsonObject = new JSONObject(new  String(responseBody));
+                    JSONArray jsonArray = jsonObject.getJSONArray("userlist");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonobject = jsonArray.getJSONObject(i);
+                        String firstName=jsonobject.getString("firstName");
+                        String lastName=jsonobject.getString("lastName");
+                        String userName=jsonobject.getString("userName");
+                        String department=jsonobject.getString("department");
+                        String email=jsonobject.getString("email");
+                        String phone=jsonobject.getString("phone");
+                        userApproval u=new userApproval();
+                        u.setFirstName(firstName);
+                        u.setLastName(lastName);
+                        u.setUserName(userName);
+                        u.setEmail(email);
+                        u.setPhone(phone);
+                        u.setDepartment(department);
+                        dataList.add(u);
+                    }
+                    mAdapter = new UserApprovalAdapter(dataList);
+                    approval.setLayoutManager(new LinearLayoutManager(getContext()));
+                    approval.setHasFixedSize(true);
+                    approval.setItemAnimator(new DefaultItemAnimator());
+                    approval.setAdapter(mAdapter);
+                    mAdapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+        return view;
+
     }
 }
